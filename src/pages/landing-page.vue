47a7234b6cc86a7f9369b704d64bee6d8483e3af
RZShachar <RZShachar@gmail.com>
2022-04-06 12:02:47 +0300

    Added more cmp files

--- src/pages/landing-page.vue
+++ src/pages/landing-page.vue
@@ -1,8 +1,27 @@
+<template>
+    <main class="landing-page-container">
+        <app-hero></app-hero>
+        <features-list></features-list>
+        <bottom-content></bottom-content>
+        <reviews-list></reviews-list>
+        <call-to-action></call-to-action>
+    </main>
+</template>
+
 <script>
 import appHero from '../cmps/app-hero.vue'
+import featuresList from '../cmps/features-list.vue'
+import bottomContent from '../cmps/bottom-content.vue'
+import reviewsList from '../cmps/reviews-list.vue'
+import callToAction from '../cmps/call-to-action.vue'
 export default {
     components: {
-        appHero
+        appHero,
+        featuresList,
+        bottomContent,
+        reviewsList,
+        callToAction
+
     }
 }
 </script>
