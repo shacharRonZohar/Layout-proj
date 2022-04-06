a8f158f78ca3032b264a10413a3c6aa03e13ded3
RZShachar <RZShachar@gmail.com>
2022-04-06 14:21:32 +0300

    Bottom-content design

--- src/cmps/bottom-content.vue
+++ src/cmps/bottom-content.vue
@@ -1,13 +1,15 @@
 <template>
-    <section class="bottom-content-container">
+    <section class="bottom-content-container full">
         <div class="background-image img"></div>
-        <h2 class="title">Stay productive, wherever you are</h2>
-        <p
-            class="upper-txt"
-        >Never let location be an issue when accessing your files. Fylo has you covered for all of your file storage needs.</p>
-        <p
-            class="bottom-txt"
-        >Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required.</p>
-        <a href="#">See how Fylo works</a>
+        <div class="content">
+            <h2 class="title">Stay productive, wherever you are</h2>
+            <p
+                class="upper-txt"
+            >Never let location be an issue when accessing your files. Fylo has you covered for all of your file storage needs.</p>
+            <p
+                class="bottom-txt"
+            >Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required.</p>
+            <a href="#">See how Fylo works</a>
+        </div>
     </section>
 </template>
