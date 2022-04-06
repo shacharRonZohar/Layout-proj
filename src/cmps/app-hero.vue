2af7b826c9e256f788ae8c4479ce5a2a1a66d415
RZShachar <RZShachar@gmail.com>
2022-04-06 11:57:55 +0300

    Finished basic hero HTML

--- src/cmps/app-hero.vue
+++ src/cmps/app-hero.vue
@@ -1,3 +1,10 @@
 <template>
-    <div></div>
+    <section class="hero-container">
+        <div class="background-img hero-img"></div>
+        <h2 class="hero-title">All your files in one secure location, accessible anywhere</h2>
+        <p
+            class="hero-txt"
+        >Fylo stores all your most important files in one secure location. Access them wherever you need, share and collaborate with friends, family, and co-workers.</p>
+        <button class="btn btn-get-started">Get Started</button>
+    </section>
 </template>
