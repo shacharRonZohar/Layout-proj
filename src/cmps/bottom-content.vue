fa37d8d6233f6c0c3d14083b67f449d9fd876aaa
RZShachar <RZShachar@gmail.com>
2022-04-06 19:50:02 +0300

    More fixes

--- src/cmps/bottom-content.vue
+++ src/cmps/bottom-content.vue
@@ -1,23 +1,25 @@
 <template>
-    <section class="bottom-content-container full">
-        <div class="img-container">
-            <img src="../assets/imgs/illustration-stay-productive.png" />
-        </div>
-        <!-- The distance on the right of the screen here should be more dynamic, its too big at certain points -->
-        <div class="content">
-            <h1 class="title">Stay productive, wherever you are</h1>
-            <p class="upper-txt">
-                Never let location be an issue when accessing your files. Fylo has you covered for
-                all of your file storage needs.
-            </p>
-            <p class="bottom-txt">
-                Securely share files and folders with friends, family and colleagues for live
-                collaboration. No email attachments required.
-            </p>
-            <a href="#">
-                See how Fylo works
-                <div class="icon"></div>
-            </a>
+    <section class="bottom-content-outer-container full">
+        <div class="bottom-content-container">
+            <div class="img-container">
+                <img src="../assets/imgs/illustration-stay-productive.png" />
+            </div>
+            <!-- There should be more space on the sides on bigger devices -->
+            <div class="content">
+                <h1 class="title">Stay productive, wherever you are</h1>
+                <p class="upper-txt">
+                    Never let location be an issue when accessing your files. Fylo has you covered for
+                    all of your file storage needs.
+                </p>
+                <p class="bottom-txt">
+                    Securely share files and folders with friends, family and colleagues for live
+                    collaboration. No email attachments required.
+                </p>
+                <a href="#">
+                    See how Fylo works
+                    <div class="icon"></div>
+                </a>
+            </div>
         </div>
     </section>
 </template>
