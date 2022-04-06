94eaef61757ced1276d808625a1e19c5a46f5f58
RZShachar <RZShachar@gmail.com>
2022-04-06 19:53:30 +0300

    More final fixes

--- src/cmps/features-list.vue
+++ src/cmps/features-list.vue
@@ -16,22 +16,22 @@ export default {
         return {
             features: [
                 {
-                    img: './src/assets/imgs/icon-access-anywhere.svg',
+                    img: '/src/assets/imgs/icon-access-anywhere.svg',
                     title: 'Access your files, anywhere',
                     txt: `The ability to use a smartphone, tablet, or computer to access your account means your files follow you everywhere`
                 },
                 {
-                    img: './src/assets/imgs/icon-security.svg',
+                    img: '/src/assets/imgs/icon-security.svg',
                     title: 'Security you can trust',
                     txt: `2-factor authentication and user-controlled encryption are just a couple of the security features we allow to help secure your files`
                 },
                 {
-                    img: './src/assets/imgs/icon-collaboration.svg',
+                    img: '/src/assets/imgs/icon-collaboration.svg',
                     title: 'Real-time collaboration',
                     txt: `Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required.`
                 },
                 {
-                    img: './src/assets/imgs/icon-any-file.svg',
+                    img: '/src/assets/imgs/icon-any-file.svg',
                     title: 'Store any type of file',
                     txt: `Whether you're sharing holidays photos or work documents, Fylo has you covered allowing for all file types to be securely stored and shared.`
                 },
