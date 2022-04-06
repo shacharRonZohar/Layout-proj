7a013ef468660a0a37a84e159e3aafa908124f5c
RZShachar <RZShachar@gmail.com>
2022-04-06 12:31:13 +0300

    Basic call-to-action HTML structure

---
+++ src/cmps/call-to-action.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="call-to-action-container">
+        <h2 class="title">Get early access today</h2>
+        <p class="txt">
+            It only takes a minute to sign up and our free starter tier is extremely generous. If you have
+            any questions, our support team would be happy to help you.
+        </p>
+        <input type="email" placeholder="email@exmaple.com" />
+        <button class="btn btn-get-started">Get Started For Free</button>
+    </div>
+</template>
